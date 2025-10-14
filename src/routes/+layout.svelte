67a6e960f8f1ed5cc3a27dc4da62e2beb2be923b
Beetroot Paul <paul@beetrootpaul.com>
2025-10-15 01:10:26 +0200

    fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,11 +6,23 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+
+	<!-- The snippet below was copied from https://fonts.google.com/ -->
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Maven+Pro:wght@400..900&display=swap"
+		rel="stylesheet"
+	/>
 </svelte:head>
 
 {@render children?.()}
 
 <style>
+	:global(html) {
+		font-family: 'Maven Pro', sans-serif;
+	}
+
 	/* Make sure there are no white margins on the top of the page */
 	:global(body) {
 		display: flex;
