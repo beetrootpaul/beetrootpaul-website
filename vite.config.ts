351323c530eadc9e7351635cd4daa37392817e1a
Beetroot Paul <paul@beetrootpaul.com>
2025-11-04 17:20:38 +0100

    setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { preview } from '@vitest/browser-preview';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	// Tell Vitest to use the `browser` entry points in `package.json` files, even though it's running in Node
+	resolve: process.env.VITEST
+		? {
+				conditions: ['browser'],
+			}
+		: undefined,
+	test: {
+		browser: {
+			provider: preview(),
+			instances: [{ browser: 'chromium' }],
+		},
+	},
 });
