055835bc4b1c5e1a1c7f23976418bb0fd46f3e2a
Beetroot Paul <paul@beetrootpaul.com>
2025-11-07 16:19:55 +0100

    multiple viewport sizes, but some issues with reference images

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,43 @@ export default defineConfig({
 		browser: {
 			enabled: true,
 			provider: playwright(),
-			instances: [{ browser: 'chromium' }],
-			viewport: { width: 1280, height: 768 },
+			instances: [
+				{
+					name: 'iPhone SE, portrait',
+					browser: 'chromium',
+					viewport: { width: 375, height: 667 },
+				},
+				{
+					name: 'iPhone SE, landscape',
+					browser: 'chromium',
+					viewport: { width: 667, height: 375 },
+				},
+				{
+					name: 'iPad Mini, portrait',
+					browser: 'chromium',
+					viewport: { width: 768, height: 1024 },
+				},
+				{
+					name: 'iPad Mini, landscape',
+					browser: 'chromium',
+					viewport: { width: 1024, height: 768 },
+				},
+				{
+					name: 'iPad Pro, portrait',
+					browser: 'chromium',
+					viewport: { width: 1024, height: 1366 },
+				},
+				{
+					name: 'iPad Pro, landscape',
+					browser: 'chromium',
+					viewport: { width: 1366, height: 1024 },
+				},
+				{
+					name: 'Full HD',
+					browser: 'chromium',
+					viewport: { width: 1920, height: 1080 },
+				},
+			],
 		},
 	},
 });
