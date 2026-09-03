073120c7d8f2149776acd30c492ed982856c1355
Beetroot Paul <paul@beetrootpaul.com>
2026-09-03 17:56:22 +0200

    Updated YouTube URL for No Longer a Hero, due to the channel migration.

--- src/lib/portfiolio_entries.ts
+++ src/lib/portfiolio_entries.ts
@@ -282,7 +282,7 @@ export const portfolioEntries: PortfolioEntry[] = [
 		slug: '145-no-longer-a-hero',
 		title: 'No Longer a Hero',
 		artwork: {
-			youtubeUrl: 'https://youtu.be/D7zTWePkF88',
+			youtubeUrl: 'https://youtube.com/shorts/n6J0nhhcCtE',
 			thumbnail: DEBUG
 				? // Debug: Non-square low image.
 					'nolongerahero_x1_debug_low.gif'
@@ -317,7 +317,7 @@ export const portfolioEntries: PortfolioEntry[] = [
 			instagramUrl:
 				'https://twitter.com/beetrootpaul/status/1500782518110466051',
 			xUrl: 'https://x.com/beetrootpaul/status/1500782518110466051',
-			youtubeUrl: 'https://youtu.be/D7zTWePkF88',
+			youtubeUrl: 'https://youtube.com/shorts/n6J0nhhcCtE',
 		},
 		progress: [
 			{
