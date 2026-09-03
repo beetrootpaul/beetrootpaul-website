be17d08417bdaee00b0c4d7791fb3eb7545f8747
Beetroot Paul <paul@beetrootpaul.com>
2026-09-03 22:50:15 +0200

    build + update test snapshots

---
+++ docs/_app/immutable/chunks/C5CZolFM.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_10w0olv?.base??"",a=globalThis.__sveltekit_10w0olv?.assets??s??"";export{a,s as b};
--- docs/_app/immutable/chunks/Dg05GJnb.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_606dss?.base??"",a=globalThis.__sveltekit_606dss?.assets??s??"";export{a,s as b};
